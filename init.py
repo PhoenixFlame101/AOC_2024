ea1305ba9a1c6699089666f93aad7bf168482123
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2024-12-14 10:02:13 +0530

    update init.py
    input parsing now added to the python scripts by default
    
    gives a list of all the lines in the input

--- init.py
+++ init.py
@@ -7,6 +7,10 @@ highest = max((int(d) for d in os.listdir('.') if os.path.isdir(
     os.path.join('.', d)) and re.match(r'^\d+', d)), default=None) + 1
 
 os.mkdir(f"{highest}")
-open(f"{highest}/{highest}.py", "w").close()
+
+with open(f"{highest}/{highest}.py", "w") as f:
+    f.write("import sys\n\n")
+    f.write(
+        "lines = [line.strip() for line in sys.stdin.read().strip().splitlines()]\n")
 open(f"{highest}/file.in", "w").close()
 open(f"{highest}/sample.in", "w").close()
